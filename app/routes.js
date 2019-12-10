fbcfe7df403e292c8c6234a0fbd06a77af2643e7
Chrissy-Barrett <32103897+Chrissy-Barrett@users.noreply.github.com>
2019-12-10 21:34:17 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,58 @@
+
 const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+
+
+// //////////////////////////////// ADD PHOTO
+
+router.get('/add-photo', function(req, res) { 
+
+res.render('add-photo');
+
+}); 
+
+
+router.post('/add-photo', function (req, res) { 
+
+res.redirect('check-photo');
+
+});
+
+
+// //////////////////////////////// CHECK PHOTO
+
+router.get('/check-photo', function(req, res) { 
+
+res.render('check-photo');
+
+}); 
+
+
+router.post('/check-photo', function (req, res) { 
+
+res.redirect('photos');
+
+});
+
+
+// //////////////////////////////// PHOTOS
+
+router.get('/photos', function(req, res) { 
+
+res.render('photos');
+
+}); 
+
+
+router.post('/photos', function (req, res) { 
+
+res.redirect('/');
+
+});
+
+
+
 
 module.exports = router
